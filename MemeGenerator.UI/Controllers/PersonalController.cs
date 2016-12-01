24b8aff15b2d3370ffebc12636273c9bc806139a
cleber <systemofcleber@gmail.com>
2016-12-01 02:05:43 -0200

    Progressive web app e pagina do criador

---
+++ MemeGenerator.UI/Controllers/PersonalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MemeGenerator.UI.Controllers
+{
+    public class PersonalController : Controller
+    {
+        // GET: Personal
+        public ActionResult Creator()
+        {
+            return View();
+        }
+    }
+}
